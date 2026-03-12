1387d4b6ae12955a3435032318d08f8181a27ffc
UdhayaDev <03.udhayakumar@gmail.com>
2026-03-12 10:59:16 +0530

    Updated portfolio UI, project description animation and resume

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Certificate slider
 new Swiper('.swiper',{
 loop:true,
 autoplay:{
@@ -6,10 +7,24 @@ delay:2500,
 slidesPerView:1
 });
 
+// Project description toggle
+
 function toggleDesc(card){
 
-let desc = card.querySelector(".project-desc")
+// close all other project descriptions
+document.querySelectorAll(".project-desc").forEach(desc=>{
+desc.classList.remove("show")
+})
 
+// add blink animation
+card.classList.add("blink")
+
+setTimeout(()=>{
+card.classList.remove("blink")
+},300)
+
+// open clicked project description
+let desc = card.querySelector(".project-desc")
 desc.classList.toggle("show")
 
 }
