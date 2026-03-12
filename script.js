0d3350febe0279f25fd9aa2e2ba331d922579d87
UdhayaDev <03.udhayakumar@gmail.com>
2026-03-12 11:07:28 +0530

    Updates project cards

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Certificate slider
 new Swiper('.swiper',{
 loop:true,
 autoplay:{
@@ -7,24 +6,10 @@ delay:2500,
 slidesPerView:1
 });
 
-// Project description toggle
-
 function toggleDesc(card){
 
-// close all other project descriptions
-document.querySelectorAll(".project-desc").forEach(desc=>{
-desc.classList.remove("show")
-})
-
-// add blink animation
-card.classList.add("blink")
-
-setTimeout(()=>{
-card.classList.remove("blink")
-},300)
-
-// open clicked project description
 let desc = card.querySelector(".project-desc")
+
 desc.classList.toggle("show")
 
 }
